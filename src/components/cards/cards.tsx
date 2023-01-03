857b2afc47ae6978065134ca2e25208ea27697ea
Robert <rjsf06@gmail.com>
2023-01-03 11:31:35 -0300

    :sparkles: feat: permitindo cards ser mais constumizado

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -5,23 +5,39 @@ interface Props {
     image: string;
     title: string;
     text: string;
+    primary: boolean
 
 }
 
-export default function Cards({ image, title, text }: Props) {
+export default function Cards({ image, title, text, primary }: Props) {
     return (
-        <div id="cards">
+        <div id="cards" style={{
+            flexDirection: primary ? "row" : "row-reverse"
+        }}>
             <div className="img">
                 <img src={image} alt="" />
             </div>
-            <div className="content">
+            <div className="content" style={{
+                background: primary ? "transparent linear-gradient(180deg, #005BD5 0%, #002E6B 100%) 0% 0% no-repeat padding-box" : "fff"
+            }}>
                 <div>
-                    <div className="dash"></div>
-                    <h3>Viagens Nacionais</h3>
+                    <div className="dash" style={{
+                        background: primary ? "#fff" : "#005BD5"
+                    }}></div>
+                    <h3 style={{
+                        color: primary ? "#fff" : "#005BD5"
+                    }}>Viagens Nacionais</h3>
                 </div>
-                <h2>{title}</h2>
-                <p>{text}</p>
-                <button>Agendar</button>
+                <h2 style={{
+                    color: primary ? "#fff" : "#005BD5"
+                }}>{title}</h2>
+                <p style={{
+                    color: primary ? "#fff" : "#005BD5"
+                }}>{text}</p>
+                <button style={{
+                    color: primary ? "#fff" : "#005BD5",
+                    border: primary ? "#fff 1px solid" : "#005BD5 1px solid"
+                }}>Agendar</button>
             </div>
         </div>
     )
