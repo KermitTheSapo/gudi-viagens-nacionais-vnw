72d2e56744c2634eccce6b8d56d56f9ce58ac1d0
Robert <rjsf06@gmail.com>
2023-01-04 16:35:45 -0300

    :sparkles: feat: mudando ordem do alert e mudando para form

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -7,11 +7,11 @@ export default function Contact() {
     const [msg, setMsg] = useState("")
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
     const send = () => {
-        if (!emailRegex.test(gmail)) {
-            alert("Email não é valido")
-            return
-        }
         if (gmail.length > 0 && about.length > 0 && msg.length > 0) {
+            if (!emailRegex.test(gmail)) {
+                alert("Email não é valido")
+                return
+            }
             alert("Enviado")
             setGmail("")
             setAbout("")
@@ -21,12 +21,12 @@ export default function Contact() {
         }
     }
     return (
-        <div id="contact">
+        <form id="contact" onSubmit={(e) => (e.preventDefault())}>
             <h3 className="contact__title">Fale conosco</h3>
             <input value={gmail} className="contact__input" type="text" placeholder="Diga o seu melhor email" onChange={(e) => setGmail(e.target.value)} />
             <input value={about} className="contact__input" type="text" placeholder="Assunto" onChange={(e) => setAbout(e.target.value)} />
             <textarea value={msg} className="contact__textarea" name="" id="" cols={30} rows={10} placeholder="Escreva a sua mensagem" onChange={(e) => setMsg(e.target.value)}></textarea>
             <button className="contact__button" role={"send"} onClick={() => send()}>Enviar</button>
-        </div>
+        </form>
     )
 }
